55f53702b28034e04df9b4868b2991fd57edeecd
MisterTB <thadbrownsc@gmail.com>
2023-04-22 04:34:48 +0000

    wk4 post confirmation lambda

--- aws/lambdas/cruddur-post-confirmation.py
+++ aws/lambdas/cruddur-post-confirmation.py
@@ -14,7 +14,7 @@ def lambda_handler(event, context):
     try:
       print('enteredTry')
       sql = f"""
-        "INSERT INTO users (
+        INSERT INTO users (
           display_name,
           email,
           handle,
@@ -25,7 +25,7 @@ def lambda_handler(event, context):
           '{user_email}',
           '{user_handle}',
           '{user_cognito_id}'
-        )"
+        )
       """
       print('SQL Statement ------')
       print(sql)
