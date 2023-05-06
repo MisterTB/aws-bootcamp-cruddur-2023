8f312c78272d2e1d917d75e89ca80acbec60d3be
MisterTB <thadbrownsc@gmail.com>
2023-05-06 18:13:32 +0000

    wk4 -tracking down errors

--- backend-flask/services/create_activity.py
+++ backend-flask/services/create_activity.py
@@ -3,6 +3,22 @@ from datetime import datetime, timedelta, timezone
 from lib.db import db
 
 class CreateActivity:
+  def create_activity(handle, message, expires_at):
+    sql = db.template('activities','create')
+    uuid = db.query_commit(sql,{
+      'handle': handle,
+      'message': message,
+      'expires_at': expires_at
+    })
+
+    return uuid
+
+  def query_object_activity(uuid):
+    sql = db.template('activities','object')
+    return db.query_object_json(sql,{
+      'uuid': uuid
+    })
+
   def run(message, user_handle, ttl):
     model = {
       'errors': None,
@@ -47,18 +63,4 @@ class CreateActivity:
 
       object_json = CreateActivity.query_object_activity(uuid)
       model['data'] = object_json
-    return model
-
-  def create_activity(handle, message, expires_at):
-    sql = db.template('activities','create')
-    uuid = db.query_commit(sql,{
-      'handle': handle,
-      'message': message,
-      'expires_at': expires_at
-    })
-    return uuid
-  def query_object_activity(uuid):
-    sql = db.template('activities','object')
-    return db.query_object_json(sql,{
-      'uuid': uuid
-    })
+    return model  
